f7ea5f574d5147111a7e3d29b5d210ecdaf5e464
Peter Liu <pedrofeo92@gmail.com>
2014-02-03 16:06:55 -0500

    Uploaded code

---
+++ PhoneApp2/PhoneApp2/AddEvent.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Runtime.Serialization;
+using Microsoft.WindowsAzure.MobileServices;
+
+namespace PhoneApp2
+{
+    public class EventItem
+    {
+        public int Id { get; set; }
+
+        [DataMember(Name = "name")]
+        public string EventName { get; set; }
+
+        [DataMember(Name = "location")]
+        public string Location { get; set; }
+
+        [DataMember(Name = "date")]
+        public string EventDate { get; set; }
+
+        [DataMember(Name = "time")]
+        public string EventTime { get; set; }
+    }
+    public partial class AddEvent : PhoneApplicationPage
+    {
+        // MobileServiceCollectionView implements ICollectionView (useful for databinding to lists) and 
+        // is integrated with your Mobile Service to make it easy to bind your data to the ListView
+        private MobileServiceCollection<EventItem, EventItem> items;
+
+        private IMobileServiceTable<EventItem> todoTable = App.MobileService.GetTable<EventItem>();
+        
+
+        public AddEvent()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+        }
+
+        private async void InsertTodoItem(EventItem todoItem)
+        {
+            // This code inserts a new TodoItem into the database. When the operation completes
+            // and Mobile Services has assigned an Id, the item is added to the CollectionView
+            await todoTable.InsertAsync(todoItem); //
+            items.Add(todoItem);
+        }
+
+
+        private void add(object sender, EventArgs e)
+        {
+            var todoItem = new EventItem { EventName = name.Text };
+            InsertTodoItem(todoItem);   
+
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+        }
+    }
+}
---
+++ PhoneApp2/PhoneApp2/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using PhoneApp2.Resources;
+using System.Device.Location;
+using Windows.Devices.Geolocation;
+
+namespace PhoneApp2
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            Time.Text = (DateTime.Now).ToString();
+
+            // db
+
+        
+        }
+
+        public async void getLocations()
+        {    
+            Geolocator geoloc = new Geolocator();
+            Geoposition pos = await geoloc.GetGeopositionAsync(TimeSpan.FromMinutes(1), TimeSpan.FromSeconds(30));
+
+            var gpsCoord = new GeoCoordinate(pos.Coordinate.Latitude, pos.Coordinate.Longitude);
+
+            // get location from database and calculate travel time
+        }
+
+        private void addEvent(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/AddEvent.xaml", UriKind.RelativeOrAbsolute));
+
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+        }
+
+        private void searchButtonClick(object sender, EventArgs e)
+        {
+            // search the database
+        }
+
+        private void calendarButtonClick(object sender, EventArgs e)
+        {
+            // open calendar app
+        }
+
+        private void mapsButtonClick(object sender, EventArgs e)
+        {
+            //
+        }
+
+    }
+}
